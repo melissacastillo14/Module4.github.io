4b775d8d831e621c52c3186b951fffd2b5f20a91
melissacastillo14 <72774252+melissacastillo14@users.noreply.github.com>
2022-04-02 15:20:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var names = new Array();
 names[0] = "Yaakov";
 names[1] = "John";
 names[2] = "Jen";
-names[3] = "Paul";
-names[4] = "Frank";
-names[5] = "Larry";
-names[6] = "Paula";
-names[7] = "Laura";
-names[8] = "Jim";
-names[9] = "Kim";
+names[3] = "Jason";
+names[4] = "Paul";
+names[5] = "Frank";
+names[6] = "Larry";
+names[7] = "Paula";
+names[8] = "Laura";
+names[9] = "Jim";
 
 
 for (var i = 0; i < names.length; i++) {
