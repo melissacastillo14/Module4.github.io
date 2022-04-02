1def8fdd19b8b7aa2c0a4d80b32dc5bb3fe149a6
melissacastillo14 <72774252+melissacastillo14@users.noreply.github.com>
2022-04-02 15:08:49 -0400

    Update script.js

--- Module 4/script.js
+++ Module 4/script.js
@@ -18,3 +18,10 @@ for (var i = 0; i < names.length; i++) {
         console.log("Hello " + names[i])
     }
 }
+
+ if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
